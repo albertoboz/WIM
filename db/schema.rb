dcd7d78f7af98b88affbf2a49027d06440132a9f
Alberto Chiorboli <alberto.chiorboli@gmail.com>
2017-04-24 15:39:54 +0200

    Create room table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424133221) do
+ActiveRecord::Schema.define(version: 20170424133835) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "room_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
